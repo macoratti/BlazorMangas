f8ba29ddbf7a629600d4c7125f19e2e4abfa77b8
Macoratti <Macoratti@DESKTOP-DK57UNP>
2023-06-21 08:46:22 -0300

    Add project files.

---
+++ BlazorMangas/Program.cs
@@ -0,0 +1,21 @@
+using Blazored.LocalStorage;
+using BlazorMangas;
+using BlazorMangas.Services.Autentica;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ApiMangas", options =>
+{
+    options.BaseAddress = new Uri("https://localhost:7020/");
+});
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+
+await builder.Build().RunAsync();
